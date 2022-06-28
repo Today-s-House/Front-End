32d00b94002e3426e672624adfaaa9504c9ec406
ryurim0109 <dbfla5036@naver.com>
2022-06-28 22:08:46 +0900

    [docs]스타일드컴포넌트 + 리덕스 툴킷 설정_220628

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,24 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/GlobalStyles";
+import theme from "../styles/theme";
+import { wrapper } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new QueryClient();
   return (
-    <QueryClientProvider client={client}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={client}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>오늘의집</title>
+      </Head>
       <div>
         <span>오늘의 집이다!!!!</span>
       </div>
-    </div>
+    </>
   );
 }
